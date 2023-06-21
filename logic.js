47a77c058caf512181e0a312a8054be947c3db0a
alexansaa <alexan.saa@gmail.com>
2023-06-21 11:00:28 -0500

    Implement event listeners

--- logic.js
+++ logic.js
@@ -1,21 +1,17 @@
-function sandwitchClick() {
-  var myIconContainer = document.getElementById('menuHamburgerContainer');
-  var myLogo = document.getElementById('homeLogo_Angosto');
-  var myIcon = document.getElementById('baraMenuImage');
-  var myIconClose = document.getElementById('closeMenuImage');
-  var myHeaderContainer = document.getElementById('topBar');
-  var myLinks = document.getElementById('myLinks');
-  if (myLinks.style.display === 'flex') {
-    myLinks.style.display = 'none';
-    myHeaderContainer.setAttribute('style', 'flex-direction: row;');
-    myIcon.style.display = 'block';
-    myIconClose.style.display = 'none';
-    myLogo.style.display = 'block';
-  } else {
-    myLinks.style.display = 'flex';
-    myHeaderContainer.setAttribute('style', 'flex-direction: column;');
-    myIcon.style.display = 'none';
-    myIconClose.style.display = 'block';
-    myLogo.style.display = 'none';
-  }
-}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const openBtn = document.querySelector('#menuHamburgerContainer');
+  const myLinksContainer = document.querySelector('#myLinks');
+  const myIcon = document.querySelector('#menuHamburgerContainer');
+  const myIconClose = document.querySelector('#closeMenuImage');
+  openBtn.addEventListener('click',() => {
+    myLinksContainer.classList.add('openable');
+    myIcon.classList.add('disaperable');
+    myIconClose.classList.add('apearable');
+  });
+  const closeBtn = document.querySelector('#closeHamburger');
+  closeBtn.addEventListener('click', () => {
+    myLinksContainer.classList.remove('openable');
+    myIcon.classList.remove('disaperable');
+  });
+});
