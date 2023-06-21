e6fcff610ff55f16e6012f6e0a20111b65e3c9c6
alexansaa <alexan.saa@gmail.com>
2023-06-21 11:03:21 -0500

    fixed 80% linters

--- logic.js
+++ logic.js
@@ -1,10 +1,9 @@
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   const openBtn = document.querySelector('#menuHamburgerContainer');
   const myLinksContainer = document.querySelector('#myLinks');
   const myIcon = document.querySelector('#menuHamburgerContainer');
   const myIconClose = document.querySelector('#closeMenuImage');
-  openBtn.addEventListener('click',() => {
+  openBtn.addEventListener('click', () => {
     myLinksContainer.classList.add('openable');
     myIcon.classList.add('disaperable');
     myIconClose.classList.add('apearable');
