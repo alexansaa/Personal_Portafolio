77fc2c4722fb2f035520e2e72614b06eb4e62f7a
alexansaa <alexan.saa@gmail.com>
2023-06-20 19:24:42 -0500

    js format fix

--- logic.js
+++ logic.js
@@ -1,27 +1,21 @@
-function sandwitchClick(){
-    var myIconContainer = document.getElementById("menuHamburgerContainer");
-    var myLogo = document.getElementById("homeLogo_Angosto");
-    var myIcon = document.getElementById("baraMenuImage");
-    var myIconClose = document.getElementById("closeMenuImage");
-    var myHeaderContainer = document.getElementById("topBar");
-    var myLinks = document.getElementById("myLinks");
-    if(myLinks.style.display === "flex"){
-        myLinks.style.display = "none";
-        //myLinks.setAttribute("order: 1;");
-        myHeaderContainer.setAttribute("style","flex-direction: row;");
-        //myIconContainer.setAttribute("order: 2;");
-        myIcon.style.display = "block";
-        myIconClose.style.display = "none";
-        myLogo.style.display = "block";
-    }else{
-        myLinks.style.display = "flex";
-        //myLinks.setAttribute("order: 2;");
-        myHeaderContainer.setAttribute("style","flex-direction: column;");
-        //myIconContainer.setAttribute("order: 1;");
-
-        //myIcon.src = "images/xMenu_50.png";
-        myIcon.style.display = "none";
-        myIconClose.style.display = "block";
-        myLogo.style.display = "none";
-    }
+function sandwitchClick() {
+  var myIconContainer = document.getElementById("menuHamburgerContainer");
+  var myLogo = document.getElementById("homeLogo_Angosto");
+  var myIcon = document.getElementById("baraMenuImage");
+  var myIconClose = document.getElementById("closeMenuImage");
+  var myHeaderContainer = document.getElementById("topBar");
+  var myLinks = document.getElementById("myLinks");
+  if (myLinks.style.display === "flex") {
+    myLinks.style.display = "none";
+    myHeaderContainer.setAttribute("style", "flex-direction: row;");
+    myIcon.style.display = "block";
+    myIconClose.style.display = "none";
+    myLogo.style.display = "block";
+  } else {
+    myLinks.style.display = "flex";
+    myHeaderContainer.setAttribute("style", "flex-direction: column;");
+    myIcon.style.display = "none";
+    myIconClose.style.display = "block";
+    myLogo.style.display = "none";
+  }
 }
