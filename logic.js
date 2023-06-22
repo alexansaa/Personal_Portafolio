1745988def6d29cf4d7d5d84f99714cdc101f73b
AdrianScharff <134537383+AdrianScharff@users.noreply.github.com>
2023-06-22 16:04:34 -0600

    Update logic.js variable definition

--- logic.js
+++ logic.js
@@ -265,8 +265,8 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 document.addEventListener('DOMContentLoaded', () => {
   function validateForm() {
-    //const myErrorMsj = document.getElementById('validationMsj');
-    //const myEmail = document.getElementById('faddress').value;
+    const myErrorMsj = document.getElementById('validationMsj');
+    const myEmail = document.getElementById('faddress').value;
     if (myEmail !== String(myEmail).toLowerCase()) {
       myErrorMsj.textContent = 'Error de Validacion: Email esta en mayusculas!';
     } else {
