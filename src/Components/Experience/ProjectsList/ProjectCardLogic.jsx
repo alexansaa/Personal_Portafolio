2b49ff8e6cef76cb9b3b8b22d32306ddc6e22620
alexansaa <alexan.saa@gmail.com>
2024-03-24 12:55:01 -0500

    implementing real projects

--- src/Components/Experience/ProjectsList/ProjectCardLogic.jsx
+++ src/Components/Experience/ProjectsList/ProjectCardLogic.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import styles from '../../../styles/ProjectCard.module.css';
 import rightArrow from '../../../assets/right_arrow.png';
 
@@ -7,7 +8,8 @@ const ProjectCardLogic = ({ project, myIndex }) => {
       <div className={styles.projectCard_img_cnt}>
         <img src={project.img} alt={project.name} />
       </div>
-      <div className={styles.projectCard_info_cnt}>
+
+      <Link className={styles.projectCard_info_cnt} to={project.live}>
         <div className={styles.projectCard_info_title}>
           <h2>{project.name}</h2>
           <img src={rightArrow} alt={`go to ${project.name} project`} />
@@ -16,14 +18,19 @@ const ProjectCardLogic = ({ project, myIndex }) => {
           <img src={project.location_img}></img>
           <p>{project.location}</p>
         </div>
-        <div>
-          <ul>
-            {project.technologies.map((tech, index) => {
-              return <li key={index}>{tech}</li>
-            })}
-          </ul>
+        <div className={styles.project_meta_data}>
+          <div>
+            <ul>
+              {project.technologies.map((tech, index) => {
+                return <li key={index}>{tech}</li>
+              })}
+            </ul>
+          </div>
+          <Link className={styles.project_meta_data_repo} to={project.repo}>
+            Go To Repo! &#11016;
+          </Link>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
